4c0ecd9df69ab8acaa06677ac6c9afcbb3bef832
Debbie Leung <dsl2162@columbia.edu>
2020-11-15 23:30:53 +0800

    Revised login page

--- server.py
+++ server.py
@@ -171,12 +171,6 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
-  error = None
-  if request.method == 'POST':
-      if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-          error = 'Invalid Credentials. Please try again.'
-      else:
-          return redirect(url_for('home'))
   return render_template('login.html', error=error, form=form)
 
 # Example of adding new data to the database
